9296c36096f8a9fa29ae77efb2996b039eeab3df
north636 <chris.davies.uk@member.mensa.org>
2014-02-14 23:12:48 +0000

    started writing a spec for Syslog::Server.

---
+++ spec/server_spec.rb
@@ -0,0 +1,37 @@
+require 'minitest/autorun'
+
+require 'syslog/server'
+
+describe Syslog::Server do
+  it 'should be asynchronous' do
+    transport = Object.new
+    class << transport
+      def read
+        sleep 10
+      end
+
+      def close
+      end
+    end
+
+    server = Syslog::Server.new(transport)
+    server.stop
+    server.stopped?.must_equal true
+  end
+
+  it 'should yield messages and call close on stop' do
+    block = MiniTest::Mock.new
+    block.expect(:call, nil, [ 123 ])
+
+    transport = MiniTest::Mock.new
+    transport.expect(:read, 123)
+    transport.expect(:close, nil)
+
+    server = Syslog::Server.new(transport) { |msg| block.call(msg) }
+    sleep 0.1
+    server.stop
+
+    block.verify
+    transport.verify
+  end
+end
