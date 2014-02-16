1e3b0eabe82c8f3ab1d9b7ad4b120c6acb59b2a9
north636 <chris.davies.uk@member.mensa.org>
2014-02-16 15:00:08 +0000

    starting work on correctly implementing the TCP transport.

--- lib/syslog/transport/tcp.rb
+++ lib/syslog/transport/tcp.rb
@@ -5,6 +5,36 @@ require 'syslog/transport/stateful_socket'
 module Syslog
   module Transport
 
+    class TCPSyslogSocket
+      def initialize(sock)
+        @sock = sock
+      end
+
+      def recv(max_len)
+        # TODO: state machine here... we can either receive an octet-counting
+        # style message or an LF-terminated message.  remember to enforce max
+        # length.
+      end
+
+      def close
+        @sock.close unless @sock.closed?
+      end
+    end
+
+    class TCPSyslogServer
+      def initialize(sock)
+        @sock = sock
+      end
+
+      def accept
+        TCPSyslogSocket.new(@sock.accept)
+      end
+
+      def close
+        @sock.close unless @sock.closed?
+      end
+    end
+
     class TCPTransport < StatefulSocketTransport
       def initialize(host_or_port, port = nil)
         if port.nil?
@@ -14,7 +44,7 @@ module Syslog
           host = host_or_port
         end
 
-        super(TCPServer.new(host, port))
+        super(TCPSyslogServer.new(TCPServer.new(host, port)))
       end
     end
 
