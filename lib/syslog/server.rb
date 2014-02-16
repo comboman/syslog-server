8e7d4714375a4d2a1167ddb461f2353717fa7a28
north636 <chris.davies.uk@member.mensa.org>
2014-02-16 14:17:28 +0000

    added convenience method for creating server.

--- lib/syslog/server.rb
+++ lib/syslog/server.rb
@@ -1,4 +1,9 @@
+require 'syslog/facility'
+require 'syslog/limit'
 require 'syslog/message'
+require 'syslog/pattern'
+require 'syslog/severity'
+require 'syslog/transport'
 
 module Syslog
   class Server
@@ -24,6 +29,19 @@ module Syslog
 
     def stopped?
       @thread.nil?
+    end
+
+    class << self
+      def self.transport(name)
+        define_method(name) do |host_or_port, port = nil, &block|
+          klass = Syslog::Transport.const_get("#{name.to_s.upcase}Transport")
+          Syslog::Server.new(klass.new(host_or_port, port), &block)
+        end
+      end
+
+      transport :tcp
+      transport :udp
+      transport :udp6
     end
   end
 end
